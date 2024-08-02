16a33c0bd9bed0e10c39752db782a7e98355b81f
lovelesh24 <lovesinghsisodiya2403@gamil.com>
2024-08-03 00:32:56 +0530

    diffrent form field rendring

--- Project4/enroll/forms.py
+++ Project4/enroll/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class StudentRgistration(forms.Form):
-    name=forms.CharField() #no data
+    name=forms.CharField(initial='lovelesh',help_text="this field contain 30 char") #no data
     email=forms.EmailField()
